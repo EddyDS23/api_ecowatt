e1f48a6231d34b995d072602fe9a017257a7bc1f
EddyDS23 <carrilloedgar408@gmail.com>
2025-11-25 18:05:07 -0600

    Modificando POOLS para evitar conexiones

--- app/database/database.py
+++ app/database/database.py
@@ -11,9 +11,10 @@ Base = declarative_base()
 
 engine = create_engine(
     settings.URL_DATABASE_SQL,
-    pool_size=20,          # Aumentamos el número de conexiones base a 20
-    max_overflow=10,       # Mantenemos el overflow
+    pool_size=5,           # Aumentamos el número de conexiones base a 20
+    max_overflow=3,        # Mantenemos el overflow
     pool_timeout=30,       # Tiempo de espera
+    pool_recycle=3600,     #Reutilizar conexiones
     pool_pre_ping=True     # Verifica que la conexión esté viva antes de usarla
 )
 
